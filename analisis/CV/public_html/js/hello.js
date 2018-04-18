475d1bff5ee3ce6e1212c3b11083102e8c8bd859
Marco Eteban Ceron Velarde <meceron4@misena.edu.co>
2018-04-18 12:10:35 -0500

    miriadaX, CV
    miriadax works and resume added

---
+++ analisis/CV/public_html/js/hello.js
@@ -0,0 +1,12 @@
+newFunction();
+
+function newFunction() {
+    var user = prompt('enter the password to enter:');
+    while (user == '' || user == null) {
+        window.alert('please,enter a paswoord.');
+        vas;
+        user = prompt('hello da');
+    }
+    document.write(time + ' ' + user);
+    window.alert('welcome' + user + '.');
+}
