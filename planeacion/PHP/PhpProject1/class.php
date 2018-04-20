eee88704e68d09ad43cedfa7e99f55781804314e
Marco Eteban Ceron Velarde <meceron4@misena.edu.co>
2018-04-20 07:52:11 -0500

    Php
    first php exercise

---
+++ planeacion/PHP/PhpProject1/class.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace myfirstprojec;
+
+class presentation {
+
+    public function say(): void {
+        echo 'Hello, my name is Marco Esteban Ceron Velarde';
+    }
+
+}
+
+
+class second {
+
+    public function second_message(): void {
+        echo 'This is my second message in PHP through object-oriented programming.';
+    }
+
+}
+
---
+++ planeacion/PHP/PhpProject1/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'class.php';
+
+use myfirstprojec\presentation;
+
+$presentation = new presentation();
+
+$presentation->say();
+
+
+
+use myfirstprojec\second;
+
+$second = new second();
+
+$second->second_message();
+
+
+
